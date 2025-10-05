48ff55f1d472fcbfcf8370c5ea177a847332d129
Justin Tadlock <justin-tadlock@pm.me>
2025-10-05 10:53:17 -0700

    Removing already configured plugins that come with LazyVim by default

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -2,57 +2,59 @@ return {
   {
     "mfussenegger/nvim-dap",
     dependencies = {
-      "leoluz/nvim-dap-go",
-      -- "mfussenegger/nvim-dap-python",
+      -- Go debugging support
+      {
+        "leoluz/nvim-dap-go",
+        config = function()
+          require("dap-go").setup()
+        end,
+      },
+      -- JavaScript/TypeScript debugging support
+      {
+        "mxsdev/nvim-dap-vscode-js",
+        dependencies = {
+          {
+            "microsoft/vscode-js-debug",
+            version = "1.74.1", -- Use a stable version
+            build = "npm ci --legacy-peer-deps && npx gulp vsDebugServerBundle && rm -rf out && mv dist out",
+          },
+        },
+        opts = {
+          debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug",
+          adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" },
+        },
+      },
     },
     config = function()
-      local dap = require("dap")
-      local os = require("os")
+      -- nvim-dap-vscode-js automatically sets up configurations for:
+      -- javascript, typescript, javascriptreact, typescriptreact
+      -- So we only need to add custom configurations if we want them
 
-      -- Refer to the following link for adapter and configurations:
-      -- https://codeberg.org/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation
+      -- local dap = require("dap")
 
-      -- JavaScript/TypeScript recommended adapter
-      -- In order to run pwa-node, we need to install vscode-js-debug (Refer to the following link for releases)
-      -- https://github.com/microsoft/vscode-js-debug/releases
-      -- I just extract the debugger to the plugins directory with the dap.lua plugin file for convenience
-      local homeDir = os.getenv("HOME")
-      dap.adapters["pwa-node"] = {
-        type = "server",
-        host = "localhost",
-        port = "${port}",
-        executable = {
-          command = "node",
-          args = { homeDir .. "/.config/nvim/lua/plugins/js-debug/src/dapDebugServer.js", "${port}" },
-        },
-      }
+      -- Only add additional custom configurations if needed
+      -- for _, language in ipairs({ "typescript", "javascript", "typescriptreact", "javascriptreact" }) do
+      --   -- Extend existing configurations (don't overwrite what nvim-dap-vscode-js created)
+      --   if not dap.configurations[language] then
+      --     dap.configurations[language] = {}
+      --   end
 
-      dap.configurations.javascript = {
-        {
-          type = "pwa-node",
-          request = "launch",
-          name = "Launch JS Debug",
-          program = "${file}",
-          cwd = "${workspaceFolder}",
-        },
-      }
-      -- Currently there is an issue with the TS configuration.
-      -- The source mappings are not correct for this DAP
-      dap.configurations.typescript = {
-        {
-          type = "pwa-node",
-          request = "launch",
-          name = "Launch TypeScript Debug",
-          program = "app.ts",
-          cwd = "${workspaceFolder/bin/**/*.js}",
-        },
-      }
-
-      -- Go
-      require("dap-go").setup()
-
-      -- Python, commenting out for now
-      -- require("dap-python").setup("python")
+      --   -- Add custom Jest configuration
+      --   table.insert(dap.configurations[language], {
+      --     type = "pwa-node",
+      --     request = "launch",
+      --     name = "Debug Jest Tests",
+      --     runtimeExecutable = "node",
+      --     runtimeArgs = {
+      --       "./node_modules/.bin/jest",
+      --       "--runInBand",
+      --     },
+      --     rootPath = "${workspaceFolder}",
+      --     cwd = "${workspaceFolder}",
+      --     console = "integratedTerminal",
+      --     internalConsoleOptions = "neverOpen",
+      --   })
+      -- end
     end,
   },
 }
